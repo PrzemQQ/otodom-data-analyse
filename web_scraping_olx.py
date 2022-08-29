f429c9ab0c14724222aeadcfb9cfa08a7e3603bc
Przemek <przemeksipa14@gmail.com>
2022-08-29 11:33:51 +0200

    olx data retrive - area column problem

--- web_scraping_olx.py
+++ web_scraping_olx.py
@@ -21,25 +21,35 @@ while True:
     soup = BeautifulSoup(html, 'html.parser')
     if soup.find("h3", text="Nie znaleziono ogłoszeń"):
         break
-    elif j ==20:
+    elif j ==3:
         break
     names = soup.find_all("h6",class_="css-v3vynn-Text eu5v0x0")
-    names_text.append(list(name.text for name in names))
+    names_text_temp = []
+    names_text_temp.append(list(name.text for name in names))
+    names_text.append(list(item for sublist in names_text_temp for item in sublist))
     
     localisations = soup.find_all("p",class_="css-p6wsjo-Text eu5v0x0")
-    localisations_text.append(list(localisation.text.split(",")[1].split("-") for localisation in localisations))
-    localisations_text = [item[0] for sublist in localisations_text for item in sublist]
+    localisations_text_temp = list(localisation.text.split(",")[1].split("-") for localisation in localisations)
+    localisations_text_temp = [localisations_text_temp[i] for i in range(len(localisations_text_temp)) if i % 2 == 0]
+
+    localisations_text.append(list(sublist[0].strip() for sublist in localisations_text_temp ))
     
     
     prices = soup.find_all("p",class_="css-wpfvmn-Text eu5v0x0")
-    prices_text.append(list(re.findall('[0-9]+', str(price.text)) for price in prices))
-    prices_text = [int("".join(substring)) for sublist in prices_text for substring in sublist ]
+    prices_text_temp = list(re.findall('[0-9]+', str(price.text)) for price in prices)
+    prices_text.append(list("".join(substring) for sublist in prices_text_temp for substring in sublist ))
     
     area = soup.find_all("p",class_="css-1bhbxl1-Text eu5v0x0")
-    areas.append(list(area.text for area in area))
-    areas = [float(area.replace("m²", "").replace(",",".").strip()) for area in areas[0]]
+    areas_temp = [area.text for area in area]
+    areas.append(list(area.replace("m²", "").replace(",",".").strip()) for area in areas_temp)
     print(areas)
-    break
+    # print(len(names_text[0]),len(localisations_text[0]),len(prices_text),len(areas[0]))
     j+=1
+   
+    
+    break
 
-
+def flatten_data(tab):
+    return list(item for sublist in tab for item in sublist)
+# data = pd.DataFrame({"Name": flatten_data(names_text), "Localisation": flatten_data(localisations_text), "Price": flatten_data(prices_text), "Area": flatten_data(areas)})
+# data.to_csv("data_olx.csv", index=False,sep=";")
