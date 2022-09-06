efaa3d3c0aa8f67193a2c615bc2ae15b0a813546
Przemek <przemeksipa14@gmail.com>
2022-09-06 22:45:02 +0200

    final scrap data

--- web_scraping_olx.py
+++ web_scraping_olx.py
@@ -13,7 +13,7 @@ prices = []
 area = []
 areas = []
 rooms = []
-
+#scraping data from olx.pl
 while True:
     url = f"https://www.olx.pl/d/nieruchomosci/mieszkania/wynajem/wroclaw/?page={j}"
     r = requests.get(url)
@@ -21,34 +21,43 @@ while True:
     soup = BeautifulSoup(html, 'html.parser')
     if soup.find("h3", text="Nie znaleziono ogłoszeń"):
         break
-    elif j ==3:
+    elif j ==25:
         break
-    names = soup.find_all("h6",class_="css-v3vynn-Text eu5v0x0")
+    names = soup.find_all("h6",class_="css-v3vynn-Text eu5v0x0") #name of the offer
     names_text_temp = []
     names_text_temp.append(list(name.text for name in names))
     names_text.append(list(item for sublist in names_text_temp for item in sublist))
     
-    localisations = soup.find_all("p",class_="css-p6wsjo-Text eu5v0x0")
-    localisations_text_temp = list(localisation.text.split(",")[1].split("-") for localisation in localisations)
-    localisations_text_temp = [localisations_text_temp[i] for i in range(len(localisations_text_temp)) if i % 2 == 0]
+    localisations = soup.find_all("p",class_="css-p6wsjo-Text eu5v0x0") #localisation of the offer
+    loc = [list(loc.text.split("-")[0].replace("Wrocław, ","").rstrip() for loc in localisations)] #remove "Wrocław, " from localisation an striping white spaces
 
-    localisations_text.append(list(sublist[0].strip() for sublist in localisations_text_temp ))
+    for l in loc[0]:
+        localisations_text.append(l) #append localisation to list
     
     
-    prices = soup.find_all("p",class_="css-wpfvmn-Text eu5v0x0")
+    prices = soup.find_all("p",class_="css-wpfvmn-Text eu5v0x0") #price of the offer
     prices_text_temp = list(re.findall('[0-9]+', str(price.text)) for price in prices)
-    prices_text.append(list("".join(substring) for sublist in prices_text_temp for substring in sublist ))
+    prices_text.append(list("".join(sublist) for sublist in prices_text_temp  )) #join list of strings into one string to create a value of price
     
-    area = soup.find_all("p",class_="css-1bhbxl1-Text eu5v0x0")
-    areas = [float(area.text.replace("m²", "").replace(",",".").strip()) for area in area]
+    area = soup.find_all("p",class_="css-1bhbxl1-Text eu5v0x0") #area of the offer
+    areas.append([(area.text.replace("m²", "").replace(",",".").strip()) for area in area])
     
-    # print(len(names_text[0]),len(localisations_text[0]),len(prices_text),len(areas[0]))
+    print(len(names_text[0]),len(localisations_text),len(prices_text[0]),len(areas))
     j+=1
+    print(localisations_text)
    
     
-    break
-
 def flatten_data(tab):
+    """_summary_: flatten list of lists into one list
+
+    Args:
+        tab (list): list of lists
+
+    Returns:
+        list: flatten list
+    """
     return list(item for sublist in tab for item in sublist)
-# data = pd.DataFrame({"Name": flatten_data(names_text), "Localisation": flatten_data(localisations_text), "Price": flatten_data(prices_text), "Area": flatten_data(areas)})
-# data.to_csv("data_olx.csv", index=False,sep=";")
+print(len(flatten_data(names_text)),len((localisations_text)),len(flatten_data(prices_text)),len(flatten_data(areas)))
+
+data = pd.DataFrame({"Name": flatten_data(names_text), "Localisation": localisations_text, "Price": flatten_data(prices_text), "Area": flatten_data(areas)}) #create dataframe
+data.to_csv("data_olx_scrap.csv", index=False,sep=";") #save data to csv file
